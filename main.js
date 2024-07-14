41e9a510952f33241f24d03664add491f1ef3c65
Tatiana Snisarenko <tatiana.snisarenko.p@gmail.com>
2024-07-14 08:54:57 +0300

    Add js

---
+++ main.js
@@ -0,0 +1,10 @@
+const modal = document.querySelector('.js-modal');
+const openBtn = document.querySelector('.js-open-btn');
+const closeBtn = document.querySelector('.js-close-btn');
+
+const toggleModal = () => {
+  modal.classList.toggle('is-open');
+};
+
+openBtn.onclick = toggleModal;
+closeBtn.onclick = toggleModal;
