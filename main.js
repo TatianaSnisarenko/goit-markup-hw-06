21eef34e45dcb76b38e864a9f5cc06e24ef2ee89
Tatiana Snisarenko <tatiana.snisarenko.p@gmail.com>
2024-07-15 13:23:38 +0300

    Before container and images

--- main.js
+++ main.js
@@ -2,9 +2,20 @@ const modal = document.querySelector('.js-modal');
 const openBtn = document.querySelector('.js-open-btn');
 const closeBtn = document.querySelector('.js-close-btn');
 
+const menu = document.querySelector('.js-menu');
+const menuOpenBtn = document.querySelector('.js-menu-open-btn');
+const menuCloseBtn = document.querySelector('.js-menu-close-btn');
+
 const toggleModal = () => {
   modal.classList.toggle('is-open');
 };
 
 openBtn.onclick = toggleModal;
 closeBtn.onclick = toggleModal;
+
+const toggleMenu = () => {
+  menu.classList.toggle('is-open');
+};
+
+menuOpenBtn.onclick = toggleMenu;
+menuCloseBtn.onclick = toggleMenu;
